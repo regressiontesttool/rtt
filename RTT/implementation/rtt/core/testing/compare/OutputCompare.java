f0a3e9b0a41c46adc2e3f7006d36f80d526a7f72
Christian Oelsner <C.Oelsner@web.de>
2014-07-27 18:16:58 +0200

    Extending error messages if nodes are not equal.

--- RTT/implementation/rtt/core/testing/compare/OutputCompare.java
+++ RTT/implementation/rtt/core/testing/compare/OutputCompare.java
@@ -9,6 +9,7 @@ import rtt.core.archive.output.ClassNode;
 import rtt.core.archive.output.Node;
 import rtt.core.archive.output.Output;
 import rtt.core.archive.output.ValueNode;
+import rtt.core.testing.compare.OutputCompare.CompareResult.DifferenceType;
 import rtt.core.testing.compare.results.TestFailure;
 
 public class OutputCompare {
@@ -30,38 +31,79 @@ public class OutputCompare {
 	}
 	
 	public static class CompareResult {
+		
+		public enum DifferenceType {
+			CLASSES("Node classes"),
+			ISINFORMATIONAL("IsInformational"),
+			ISNULL("IsNull"),
+			GEN_NAME("Generator name"),
+			GEN_TYPE("Generator type");
+			
+			private String description;
+			
+			private DifferenceType(String description) {
+				this.description = description;
+			}
+			
+			public String getDescription(Object expected, Object actual) {
+				StringBuilder builder = new StringBuilder(description);
+				builder.append(" expected = '");
+				builder.append(expected.toString());
+				builder.append("', but was '");
+				builder.append(actual.toString());
+				builder.append("'.");
+				
+				return builder.toString();
+			}
+		}		
+		
 		private String difference;
 		
 		public void setDifference(String difference) {
 			this.difference = difference;
 		}
-		
+
 		public String getDifference() {
 			return difference;
 		}
-
+		
 		public boolean hasDifferences() {
 			return difference != null && !difference.equals("");
 		}
 		
-		public static CompareResult create(String difference) {
+		public static CompareResult create(DifferenceType type, Node expected, Node actual) {
+			String message = "";
+			switch (type) {
+			case CLASSES:
+				message = type.getDescription(expected.getClass(), actual.getClass());
+				break;
+			case GEN_NAME:
+				message = type.getDescription(expected.getGeneratorName(), actual.getGeneratorName());
+				break;
+			case GEN_TYPE:
+				message = type.getDescription(expected.getGeneratorType().name(), actual.getGeneratorType().name());
+				break;
+			case ISINFORMATIONAL:
+				message = type.getDescription(expected.isInformational(), actual.isInformational());
+				break;
+			case ISNULL:
+				message = type.getDescription(expected.isIsNull(), actual.isIsNull());
+				break;
+			}
+
+			return create(message);	
+		}
+		
+		public static CompareResult create(String message) {
 			CompareResult result = new CompareResult();
-			result.setDifference(difference);
-			
-			return result;			
+			result.difference = message;
+			return result;
 		}
 	}
 	
 	
-	private static final String NODE_NULL = "One or both given nodes were null.";
-	
-	private static final String INFORMATIONAL_UNEQUAL = "One node is informational, but the other not.";
-	private static final String GEN_TYPE_UNEQUAL = "Generator types are not equal.";
-	private static final String GEN_NAME_UNEQUAL = "Generator names are not equal.";
-	private static final String ISNULL_UNEQUAL = "One node is 'isNull', but the other not.";
-	private static final String CLASSES_UNEQUAL = "The node types are not equals";
-	
-	private static final String SIZE_UNEQUAL = "The sizes of reference and actual output are not equal";
+	private static final String NODE_NULL = "One or both given nodes were null.";	
+	private static final String SIZE_UNEQUAL = "The amount of reference and actual nodes are not equal.";
 	
 	private Map<Class<? extends Node>, ExtendedComparator<?>> comparerMap;
 	private boolean testInformational;
@@ -121,25 +163,25 @@ public class OutputCompare {
 		}
 		
 		if (referenceNode.isInformational() != actualNode.isInformational()) {
-			return CompareResult.create(INFORMATIONAL_UNEQUAL);
+			return CompareResult.create(DifferenceType.ISINFORMATIONAL, referenceNode, actualNode);
 		}
 		
 		if (!referenceNode.isInformational() || testInformational) {
 			
 			if (referenceNode.getClass() != actualNode.getClass()) {
-				return CompareResult.create(CLASSES_UNEQUAL);
+				return CompareResult.create(DifferenceType.CLASSES, referenceNode, actualNode);
 			}
 			
 			if (!referenceNode.getGeneratorType().equals(actualNode.getGeneratorType())) {
-				return CompareResult.create(GEN_TYPE_UNEQUAL);
+				return CompareResult.create(DifferenceType.GEN_TYPE, referenceNode, actualNode);
 			}
 
 			if (!referenceNode.getGeneratorName().equals(actualNode.getGeneratorName())) {
-				return CompareResult.create(GEN_NAME_UNEQUAL);
+				return CompareResult.create(DifferenceType.GEN_NAME, referenceNode, actualNode);
 			}			
 
 			if (referenceNode.isIsNull() != actualNode.isIsNull()) {
-				return CompareResult.create(ISNULL_UNEQUAL);
+				return CompareResult.create(DifferenceType.ISNULL, referenceNode, actualNode);
 			}
 			
 			ExtendedComparator<?> comparer = getComparer(referenceNode.getClass());
@@ -158,150 +200,4 @@ public class OutputCompare {
 		
 		return null;
 	}
-	
-	
-
-//	private List<TestFailure> compareMatching(Output was,
-//			Output expected, boolean testInformational) {
-//		LinkedList<TestFailure> result = new LinkedList<TestFailure>();
-//
-//		List<Tree> wasTrees = new LinkedList<Tree>(was.getTree());
-//		List<Tree> expTrees = new LinkedList<Tree>(expected.getTree());
-//		Map<Tree, Tree> tests = new HashMap<Tree, Tree>();
-//
-//		// testen, ob gleichviele bäume zurückgegeben wurden
-//
-//		if (wasTrees.size() > expTrees.size())
-//			result.add(new TestFailure(wasTrees.size() - expTrees.size()
-//					+ " more trees than expected."));
-//
-//		if (wasTrees.size() < expTrees.size())
-//			result.add(new TestFailure(expTrees.size() - wasTrees.size()
-//					+ " more trees expected."));
-//
-//		RTTLogging.info("Searching for matches");
-//
-//		while (wasTrees.size() > 0) {
-//			Tree w = wasTrees.get(0);
-//			double minDistance = Double.MAX_VALUE;
-//
-//			for (Tree e : expTrees) {
-//				double curDistance = calculateEditDistance(w, e);
-//				if (curDistance < minDistance) {
-//					minDistance = curDistance;
-//					tests.put(w, e);
-//				}
-//				if (curDistance == 0)
-//					break;
-//			}
-//
-//			wasTrees.remove(w);
-//			expTrees.remove(tests.get(w));
-//
-//		}
-//
-//		for (Map.Entry<Tree, Tree> entry : tests.entrySet()) {
-//			TestFailure failure = compareTree(entry.getKey(), entry
-//					.getValue(), testInformational);
-//			if (failure != null)
-//				result.add(failure);
-//		}
-//
-//		return result;
-//	}
-
-//	private TestFailure compareTree(Tree was, Tree expected,
-//			boolean testInformational) {
-//		Iterator<Node> wasIterator = was.getNode().iterator();
-//		Iterator<Node> expIterator = expected.getNode().iterator();
-//
-//		while (wasIterator.hasNext()) {
-//			Node wasNode = wasIterator.next();
-//			NodePath curPath = new NodePath(wasNode);
-//			if (!expIterator.hasNext()) {
-//				return new TestFailure(wasNode, null, curPath,
-//						null);
-//			}
-//
-//			Node expNode = expIterator.next();
-//			TestFailure failure = compareNode(wasNode, expNode, 
-//					curPath, testInformational);
-//			if (failure != null)
-//				return failure;
-//
-//		}
-//
-//		if (expIterator.hasNext()) {
-//			return new TestFailure(null, expIterator.next(), 
-//					null, null);
-//		}
-//
-//		return null;
-//	}
-
-//	private TestFailure compareNode(Node was, Node expected, 
-//			NodePath curPath, boolean testInformational) {
-//
-//		if (was == null || expected == null) {
-//			return new TestFailure("Generated node, expected node or both nodes were null.");
-//		}
-//		
-//		if (expected.getAttributes() == null && was.getAttributes() == null) {
-//			return null;
-//		}
-//		
-//		List<Attribute> expectedAttributes = expected.getAttributes();
-//		List<Attribute> wasAttributes = was.getAttributes();
-//		
-//		if (wasAttributes.size() != expectedAttributes.size())
-//			return new TestFailure(was, expected, curPath, null);
-//
-//		for (int i = 0; i < wasAttributes.size(); ++i) {
-//			Attribute wasAttrib = wasAttributes.get(i);
-//			Attribute expAttrib = expectedAttributes.get(i);
-//
-//			if (wasAttrib.isInformational() && !testInformational)
-//				continue;
-//
-//			if (!wasAttrib.getName().equals(expAttrib.getName()))
-//				return new TestFailure(was, expected, curPath,
-//						i);
-//			if (!wasAttrib.getValue().equals(expAttrib.getValue()))
-//				return new TestFailure(was, expected, curPath,
-//						i);
-//
-//		}
-//		if ((was.getNodes() == null) != (expected.getNodes() == null))
-//			return new TestFailure(was, expected, curPath, null);
-//
-//		int maxChildren = (was.getNodes() == null) ? 0 : Math.max(was
-//				.getNodes().size(), expected.getNodes().size());
-//
-//		for (int i = 0; i < maxChildren; ++i) {
-//			Node wasChild = null;
-//			Node expChild = null;
-//			try {
-//				wasChild = was.getNodes().get(i);
-//			} catch (IndexOutOfBoundsException e) {
-//			}
-//
-//			try {
-//				expChild = expected.getNodes().get(i);
-//			} catch (IndexOutOfBoundsException e) {
-//			}
-//
-//			if (wasChild == null || expChild == null)
-//				return new TestFailure(wasChild, expChild, curPath, null);
-//
-//			TestFailure failure = compareNode(wasChild, expChild, 
-//					curPath.concat(wasChild), testInformational);
-//
-//			if (failure != null)
-//				return failure;
-//
-//		}
-//
-//		return null;
-//	}
-
 }
