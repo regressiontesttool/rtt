0b6b93eba7111b1afe1b8494911f2c2320b3e04e
Christian Oelsner <C.Oelsner@web.de>
2014-08-22 14:56:42 +0200

    Extending node creation methods to be more comprehensible.

--- RTT/tests/src/rtt/core/tests/junit/compare/CompareNodeInformationalTests.java
+++ RTT/tests/src/rtt/core/tests/junit/compare/CompareNodeInformationalTests.java
@@ -5,74 +5,20 @@ import org.junit.Test;
 
 import rtt.core.archive.output.Element;
 import rtt.core.archive.output.Node;
-import rtt.core.archive.output.Type;
 import rtt.core.archive.output.Value;
 import rtt.core.testing.compare.OutputCompare;
 import rtt.core.testing.compare.OutputCompare.CompareResult.Difference;
+import rtt.core.tests.junit.compare.CompareNodeTests.ChildType;
+import rtt.core.tests.junit.compare.CompareNodeTests.CreateInfo;
 import rtt.core.tests.junit.utils.CompareUtils;
 
 public class CompareNodeInformationalTests {
-	
-	public enum ChildType {
-		ELEMENT, VALUE, NODE;		
-	}
-
-	private static final String NAME = "aName";
-	private static final Type TYPE = Type.OBJECT;
-	private static final String FULL_NAME = "aFullName";
-	private static final String SIMPLE_NAME = "aSimpleName";
 	
 	private OutputCompare comparer;
 	
 	@Before
 	public void setUp() throws Exception {
 		comparer = new OutputCompare(true);
-	}
-	
-	private Node createNode(String name, Type type, String fullName, String simpleName, boolean informational) {
-		Node node = new Node();
-		node.setGeneratorName(name);
-		node.setGeneratorType(type);
-		node.setInformational(informational);
-		node.setFullName(fullName);
-		node.setSimpleName(simpleName);
-		
-		return node;
-	}
-	
-	private Node createSampleNode(boolean informational) {
-		return createNode(NAME, TYPE, FULL_NAME, SIMPLE_NAME, informational);
-	}
-	
-	private Node createSampleNode(int childCount, ChildType type, 
-			boolean informational) {		
-		return createSampleNode(childCount, type, informational, false);
-	}
-	
-	private Node createSampleNode(int childCount, ChildType type, 
-			boolean informational, boolean childInfos) {
-		
-		Node node = createSampleNode(informational);
-		
-		for(int i = 0; i < childCount; i++) {
-			Element childElement = null;
-			
-			switch (type) {
-			case ELEMENT:
-				childElement = CompareElementTests.createSampleElement(childInfos);
-				break;
-			case VALUE:
-				childElement = CompareValueTests.createSampleValue(childInfos);
-				break;			
-			case NODE:
-				childElement = createSampleNode(childInfos);
-				break;
-			}
-			
-			node.getElement().add(childElement);
-		}
-		
-		return node;		
 	}
 	
 	private void testThrowsException(Element referenceElement, Element actualElement, Class<? extends Throwable> expectedException) {
@@ -90,43 +36,43 @@ public class CompareNodeInformationalTests {
 	@Test
 	public void testNullNodes() throws Exception {
 		testThrowsException(null, null, IllegalArgumentException.class);		
-		testThrowsException(createSampleNode(false), null, IllegalArgumentException.class);
-		testThrowsException(null, createSampleNode(false), IllegalArgumentException.class);
+		testThrowsException(CompareNodeTests.createSampleNode(false), null, IllegalArgumentException.class);
+		testThrowsException(null, CompareNodeTests.createSampleNode(false), IllegalArgumentException.class);
 		
 		testThrowsException(null, null, IllegalArgumentException.class);		
-		testThrowsException(createSampleNode(true), null, IllegalArgumentException.class);
-		testThrowsException(null, createSampleNode(true), IllegalArgumentException.class);
+		testThrowsException(CompareNodeTests.createSampleNode(true), null, IllegalArgumentException.class);
+		testThrowsException(null, CompareNodeTests.createSampleNode(true), IllegalArgumentException.class);
 	}
 	
 	// Attribute tests
 	
 	@Test
 	public void testUnequalFullNameAttribute() throws Exception {
-		Node changedNode = createSampleNode(false);
+		Node changedNode = CompareNodeTests.createSampleNode(false);
 		changedNode.setFullName("anOtherFullName");
 		
-		testDifference(createSampleNode(false), changedNode, Difference.FULLNAME);
-		testDifference(changedNode, createSampleNode(false), Difference.FULLNAME);
+		testDifference(CompareNodeTests.createSampleNode(false), changedNode, Difference.FULLNAME);
+		testDifference(changedNode, CompareNodeTests.createSampleNode(false), Difference.FULLNAME);
 		testNoDifferences(changedNode, changedNode);
 		
 		changedNode.setInformational(true);
-		testDifference(createSampleNode(true), changedNode, Difference.FULLNAME);
-		testDifference(changedNode, createSampleNode(true), Difference.FULLNAME);
+		testDifference(CompareNodeTests.createSampleNode(true), changedNode, Difference.FULLNAME);
+		testDifference(changedNode, CompareNodeTests.createSampleNode(true), Difference.FULLNAME);
 		testNoDifferences(changedNode, changedNode);
 	}
 	
 	@Test
 	public void testUnequalSimpleNameAttribute() throws Exception {
-		Node changedNode = createSampleNode(false);
+		Node changedNode = CompareNodeTests.createSampleNode(false);
 		changedNode.setSimpleName("anOtherSimpleName");
 		
-		testDifference(createSampleNode(false), changedNode, Difference.SIMPLENAME);
-		testDifference(changedNode, createSampleNode(false), Difference.SIMPLENAME);
+		testDifference(CompareNodeTests.createSampleNode(false), changedNode, Difference.SIMPLENAME);
+		testDifference(changedNode, CompareNodeTests.createSampleNode(false), Difference.SIMPLENAME);
 		testNoDifferences(changedNode, changedNode);
 		
 		changedNode.setInformational(true);
-		testDifference(createSampleNode(true), changedNode, Difference.SIMPLENAME);
-		testDifference(changedNode, createSampleNode(true), Difference.SIMPLENAME);
+		testDifference(CompareNodeTests.createSampleNode(true), changedNode, Difference.SIMPLENAME);
+		testDifference(changedNode, CompareNodeTests.createSampleNode(true), Difference.SIMPLENAME);
 		testNoDifferences(changedNode, changedNode);
 	}
 	
@@ -135,55 +81,55 @@ public class CompareNodeInformationalTests {
 	@Test
 	public void testChildDiffsForInformationalNode() throws Exception {
 		// child diffs because of unequal child count
-		testDifference(createSampleNode(3, ChildType.VALUE, true), 
-				createSampleNode(2, ChildType.VALUE, true), Difference.CHILD_COUNT);
+		testDifference(CompareNodeTests.createSampleNode(3, ChildType.VALUE, CreateInfo.PARENT), 
+				CompareNodeTests.createSampleNode(2, ChildType.VALUE, CreateInfo.PARENT), Difference.CHILD_COUNT);
 		
 		// child diffs because of unequal child classes
-		testDifference(createSampleNode(3, ChildType.ELEMENT, true), 
-				createSampleNode(3, ChildType.VALUE, true), Difference.CLASSES);
+		testDifference(CompareNodeTests.createSampleNode(3, ChildType.ELEMENT, CreateInfo.PARENT), 
+				CompareNodeTests.createSampleNode(3, ChildType.VALUE, CreateInfo.PARENT), Difference.CLASSES);
 		
 		// child diffs because of unequal child informational types
-		testDifference(createSampleNode(3, ChildType.ELEMENT, true, true), 
-				createSampleNode(3, ChildType.ELEMENT, true), Difference.INFORMATIONAL);
+		testDifference(CompareNodeTests.createSampleNode(3, ChildType.ELEMENT, CreateInfo.PARENT_AND_CHILDS), 
+				CompareNodeTests.createSampleNode(3, ChildType.ELEMENT, CreateInfo.PARENT), Difference.INFORMATIONAL);
 	}
 	
 	@Test
 	public void testValueChild() throws Exception {
 		// unequal informational states of child value(s)
-		testDifference(createSampleNode(2, ChildType.VALUE, false),
-				createSampleNode(2, ChildType.VALUE, false, true), Difference.INFORMATIONAL);
-		testDifference(createSampleNode(2, ChildType.VALUE, false, true),
-				createSampleNode(2, ChildType.VALUE, false), Difference.INFORMATIONAL);
+		testDifference(CompareNodeTests.createSampleNode(2, ChildType.VALUE, CreateInfo.NONE),
+				CompareNodeTests.createSampleNode(2, ChildType.VALUE, CreateInfo.CHILDS), Difference.INFORMATIONAL);
+		testDifference(CompareNodeTests.createSampleNode(2, ChildType.VALUE, CreateInfo.CHILDS),
+				CompareNodeTests.createSampleNode(2, ChildType.VALUE, CreateInfo.NONE), Difference.INFORMATIONAL);
 	}
 	
 	@Test
 	public void testChildValues() throws Exception {
-		testNoDifferences(createSampleNode(1, ChildType.VALUE, false),
-				createSampleNode(1, ChildType.VALUE, false));
+		testNoDifferences(CompareNodeTests.createSampleNode(1, ChildType.VALUE, CreateInfo.NONE),
+				CompareNodeTests.createSampleNode(1, ChildType.VALUE, CreateInfo.NONE));
 		
-		Node node = createSampleNode(false);
+		Node node = CompareNodeTests.createSampleNode(false);
 		Value value = CompareValueTests.createSampleValue(false);
 		value.setValue("anOtherValue");
 		node.getElement().add(value);
 		node.getElement().add(CompareValueTests.createSampleValue(false));
 		
-		testDifference(createSampleNode(2, ChildType.VALUE, false), node, Difference.VALUE);
-		testDifference(node, createSampleNode(2, ChildType.VALUE, false), Difference.VALUE);
+		testDifference(CompareNodeTests.createSampleNode(2, ChildType.VALUE, CreateInfo.NONE), node, Difference.VALUE);
+		testDifference(node, CompareNodeTests.createSampleNode(2, ChildType.VALUE, CreateInfo.NONE), Difference.VALUE);
 	}
 	
 	@Test
 	public void testChildValuesInformational() throws Exception {		
-		testNoDifferences(createSampleNode(1, ChildType.VALUE, false, true), 
-				createSampleNode(1, ChildType.VALUE, false, true));
+		testNoDifferences(CompareNodeTests.createSampleNode(1, ChildType.VALUE, CreateInfo.CHILDS), 
+				CompareNodeTests.createSampleNode(1, ChildType.VALUE, CreateInfo.CHILDS));
 		
-		Node node = createSampleNode(false);
+		Node node = CompareNodeTests.createSampleNode(false);
 		Value value = CompareValueTests.createSampleValue(true);
 		value.setValue("anOtherValue");
 		node.getElement().add(value);
 		node.getElement().add(CompareValueTests.createSampleValue(true));
 		
-		testDifference(createSampleNode(2, ChildType.VALUE, false, true), node, Difference.VALUE);
-		testDifference(node, createSampleNode(2, ChildType.VALUE, false, true), Difference.VALUE);
+		testDifference(CompareNodeTests.createSampleNode(2, ChildType.VALUE, CreateInfo.CHILDS), node, Difference.VALUE);
+		testDifference(node, CompareNodeTests.createSampleNode(2, ChildType.VALUE, CreateInfo.CHILDS), Difference.VALUE);
 	}
 	
 	
