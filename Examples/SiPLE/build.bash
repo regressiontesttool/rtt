6d56b2606b487c623c7712b20eb1e1b8540bf3a8
cbuerger <christoff.buerger@gmx.net>
2014-11-28 13:35:56 +0100

    SiPLE Example: Added updated build scripts & added license.

--- Examples/SiPLE/build.bash
+++ Examples/SiPLE/build.bash
@@ -5,7 +5,8 @@
 
 # author: C. Bürger
 
-clear_all(){
+clean(){
+	rm siple.jar
 	rm -rf src-gen
 	rm -rf java-bin
 	mkdir src-gen
@@ -17,20 +18,21 @@ make_syntax(){
 	java -jar tools/beaver.jar -c -d src-gen specifications/Parser.beaver
 }
 
+make_semantic(){
+	# BEWARE: Because of buggy JastAdd visit checks (v 2.1.10) false-positive circularity exceptions are thrown!
+	java -jar tools/jastadd2.jar --rewrite="" --visitCheck="false" --package="siple.ast" --o=src-gen \
+		specifications/*.ast specifications/*.jrag
+}
+
 make_binary(){
 	javac -deprecation -encoding utf-8 -classpath tools/beaver-rt.jar \
 		-d java-bin -sourcepath src-gen src/**/*.java src-gen/**/*.java
 	binaries=`find java-bin/*`
-	jar cfm $1 manifest.txt -C java-bin ${binaries#*/}
+	jar cfm siple.jar manifest.txt -C java-bin ${binaries#*/}
 }
 
-# Delete jars:
-rm *.jar
-
 # Build:
-clear_all
+clean
 make_syntax
-# BEWARE: Because of buggy JastAdd visit checks (v 2.1.10) false-positive circularity exceptions are thrown!
-java -jar tools/jastadd2.jar --rewrite="" --visitCheck="false" --package="siple.ast" --o=src-gen \
-	specifications/*.ast specifications/*.jrag
-make_binary siple.jar
+make_semantic
+make_binary
